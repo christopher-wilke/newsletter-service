5b0822cbcd67d74adef78fc496c089979885da9e
Christopher Wilke <chriwil@microsoft.com>
2022-04-08 09:54:40 +0200

    wip: testing concept

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,27 @@
 use actix_web::{Responder, HttpResponse};
 use newsletter_service::*;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
+#[tokio::test]
+async fn health_check_works()  {
+    // Arange
+    spawn_app().await.expect("Failed to spawn our App");
+
+    // Need to bring 'reqwest' to perform HTTP requests against the app
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+                    .get("http://127.0.0.1:8000/health_check")
+                    .send()
+                    .await
+                    .expect("Failed to execute request");
+    
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// launch the app in the background somehow 
+async fn spawn_app() -> std::io::Result<()> {
+    todo!()
 }
