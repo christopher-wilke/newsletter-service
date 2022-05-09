6d7ac06c6038497fcfe92e62b012c67a6da39510
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-05-09 19:20:02 +0000

    added tracing for POST request

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, web, Responder};
 use chrono::Utc;
 use sqlx::{PgPool};
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,24 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection: web::Data<PgPool>
 ) -> impl Responder {
+
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscribe_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    // We do not call '.enter' on query_span!
+    // '.instrument' takes care of it at the right moments in the query future lifetime
+
+    let query_span = tracing::info_span!(
+        "Saving new sbuscriber details in the database"
+    );
 
     // 'Result' has two variants: 'Ok' and 'Err'.
     // The first for successes, the second for failures.
@@ -28,10 +47,17 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection.get_ref())
+    .instrument(query_span)
     .await {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New sbuscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query, {}", e);
+            tracing::error!("request_id {} - Failed to execute query, {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         } 
     }    
