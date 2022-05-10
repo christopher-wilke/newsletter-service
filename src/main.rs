e874e4d19a57f8d5e1466a8fb69f31bd63c3764b
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-05-10 10:28:52 +0000

    added custom subscriber for traces

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,38 @@ mod startup;
 use newsletter_service::configuration::{get_configuration, DatabaseSettings};
 use sqlx::{PgPool, PgConnection, Connection, Pool, Postgres, Executor};
 use startup::run;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, Registry, prelude::__tracing_subscriber_SubscriberExt};
 use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect all `log`s events to our subscriber
+    LogTracer::init().expect("failed to set logger");
+
+    // We are falling back to printing all spans at info-level or above
+    // if the RUST_LOG env var has not been set for us
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new(
+        "newsletter_layer".into(),
+        std::io::stdout
+    );
+
+    // The 'with' method is provided by `SubscriberExt`, an extensions
+    // trait for `Subscriber` exposed by `tracing_subscriber`
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // `set_global_default` can be used by applications to specify what subscriber should be
+    // used to process spans.
+    set_global_default(subscriber).expect("Fauled to set subscriber");
 
     let mut configuration = get_configuration().expect("Failed to read the configuration");
     configuration.database.database_name = Uuid::new_v4().to_string();
