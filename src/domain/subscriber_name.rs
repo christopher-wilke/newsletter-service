00bf023b0145705adda406ee1c563ff19c4d275d
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-05-17 13:25:04 +0000

    added some domain tests

--- src/domain/subscriber_name.rs
+++ src/domain/subscriber_name.rs
@@ -43,7 +43,7 @@ impl AsRef<str> for SubscriberName {
 
 #[cfg(test)]
 mod tests {
-    use claim::assert_ok;
+    use claim::{assert_ok, assert_err};
 
     use super::*;
 
@@ -52,4 +52,39 @@ mod tests {
         let name = "e".repeat(255);
         assert_ok!(SubscriberName::parse(name));
     }
+
+    #[test]
+    fn a_name_longer_than_256_graphmemse_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+        let name = name.to_string();
+        assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+        
+
+    
 }
