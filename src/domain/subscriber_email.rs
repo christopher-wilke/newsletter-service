804510caa78f316e37e7a5e215b02dc1a28010cc
Christopher Wilke <christopher.wilke86@googlemail.com>
2022-05-18 11:18:36 +0000

    TryFrom FormData for NewSubscriber

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -24,16 +24,8 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
-    // We are imprting the `SafeEmail` faker
-    // We also need the `Fake` trait to get access to the `.fake` method on `SafeEmail`
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
-
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claim::assert_ok!(SubscriberEmail::parse(email));
-    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -49,7 +41,22 @@ mod tests {
 
     #[test]
     fn email_missing_subject_is_rejected() {
-        let email = "@domain".to_string();
+        let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,12 +3,22 @@ use chrono::Utc;
 use sqlx::{PgPool};
 use uuid::Uuid;
 use unicode_segmentation::UnicodeSegmentation;
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -21,23 +31,24 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
 
     sqlx::query!(
-            r#"
-            INSERT INTO subscriptions (id, email, name, subscribed_at)
-            VALUES ($1, $2, $3, $4)
-            "#,
-            Uuid::new_v4(),
-            new_subscriber.email,
-            new_subscriber.name.as_ref(),
-            Utc::now()
-            )
-            .execute(pool)
-            .await
-            .map_err(|e| {
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
+        Utc::now()
+        )
+        .execute(pool)
+        .await
+        .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}", e);
             e
-        })?;
-     Ok(())
-        
+        }
+    )?;
+
+    Ok(())
 }
 
 pub async fn subscribe(
@@ -45,20 +56,16 @@ pub async fn subscribe(
     connection: web::Data<PgPool>
 ) -> HttpResponse {
 
-    let new_subscriber_name = SubscriberName::parse(form.0.name);
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
+        Err(_) => return HttpResponse::BadRequest().finish()
+    };
 
-    if new_subscriber_name.is_ok() {
-        let new_subscriber = NewSubscriber {
-            email: form.0.email,
-            name: new_subscriber_name.unwrap()
-        };
-        match insert_subscriber(&connection, new_subscriber).await {
-            Ok(_) => HttpResponse::Ok().finish(),
-            Err(_) => HttpResponse::InternalServerError().finish()
-        }   
-    } else {
-        HttpResponse::InternalServerError().finish()
+    match insert_subscriber(&connection, new_subscriber).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
+
 }
 
 /// Returns `true` if the input satifies all our validation constraints
